307211cc6db2963a511a60947d869b902eb89931
Forrest Stanley <1521744+fstanley@users.noreply.github.com>
2020-07-31 22:28:40 -0700

    naive solve

--- puzzles/easy/chuck-norris/src/main.rs
+++ puzzles/easy/chuck-norris/src/main.rs
@@ -1,11 +1,5 @@
 use std::io;
 
-use primal::Sieve;
-
-macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
-}
-
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -13,11 +7,52 @@ macro_rules! parse_input {
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let message = input_line.trim_right().to_string();
+    let message = input_line.trim_end().to_string();
 
-    // Write an action using println!("message...");
-    // To debug: eprintln!("Debug message...");
+    let binary_string = to_bytes(&message);
+    let mut chuck_norris_string = String::new();
 
-    println!("answer");
+    let mut current_value = binary_string.chars().next().unwrap();
+    let mut count = 0;
+    let mut it = binary_string.chars().peekable();
+    while let Some(c) = it.next() {
+        if c == current_value {
+            count += 1;
+        }
+        if c != current_value {
+            chuck_norris_string.push_str(match current_value {
+                '1' => " 0 ",
+                _ => " 00 ",
+            });
+            for _n in 0..count {
+                chuck_norris_string.push_str("0");
+            }
+            current_value = c;
+            count = 1;
+        }
+        if it.peek().is_none() {
+            chuck_norris_string.push_str(match current_value {
+                '1' => " 0 ",
+                _ => " 00 ",
+            });
+            for _n in 0..count {
+                chuck_norris_string.push_str("0");
+            }
+            current_value = c;
+            count = 1;
+        }
+    }
+    let chuck_norris_string = chuck_norris_string.trim();
+    // println!("{}", binary_string);
+    println!("{}", chuck_norris_string);
 }
 
+fn to_bytes(input_string: &str) -> String {
+    let mut binary_string = String::new();
+    for b in input_string.as_bytes() {
+        eprintln!("{:07b}", b);
+        let b = format!("{:07b}", b);
+        binary_string.push_str(&b)
+    }
+    binary_string
+}
